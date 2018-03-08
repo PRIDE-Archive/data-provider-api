f61711c565587d1a499d6765ba9034cc2a558031
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-03-08 12:25:19 +0000

    Constructor added to DefaultContact

--- src/main/java/uk/ac/ebi/pride/archive/dataprovider/user/DefaultContact.java
+++ src/main/java/uk/ac/ebi/pride/archive/dataprovider/user/DefaultContact.java
@@ -3,7 +3,8 @@ package uk.ac.ebi.pride.archive.dataprovider.user;
 import uk.ac.ebi.pride.archive.dataprovider.utils.TitleConstants;
 
 /**
- * The DefaultContact is the default implementation of the {@link ContactProvider} interface. 
+ * The DefaultContact is the default implementation of the {@link ContactProvider} interface.
+ *
  * @author Yasset Perez-Riverol
  * @version $Id$
  */
@@ -32,6 +33,43 @@ public class DefaultContact implements ContactProvider{
 
     /** ORCID ID **/
     private String orcid;
+
+    /**
+     * Dafault Constructor for ContactProvider
+     * @param title Title
+     * @param firstName FirstName
+     * @param lastName LastName
+     * @param identifier Identifier
+     * @param affiliation Affiliation
+     * @param email Email
+     * @param country Country
+     * @param orcid ORCID
+     */
+    public DefaultContact(TitleConstants title, String firstName, String lastName,
+                          String identifier, String affiliation, String email, String country, String orcid) {
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.identifier = identifier;
+        this.affiliation = affiliation;
+        this.email = email;
+        this.country = country;
+        this.orcid = orcid;
+    }
+
+    /**
+     * The minimum information for a Contact.
+     * @param firstName FirstName
+     * @param lastName LastName
+     * @param affiliation Affiliation
+     * @param country Country
+     */
+    public DefaultContact(String firstName, String lastName, String affiliation, String country) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.affiliation = affiliation;
+        this.country = country;
+    }
 
     @Override
     public TitleConstants getTitle() {
@@ -71,5 +109,37 @@ public class DefaultContact implements ContactProvider{
     @Override
     public String getOrcid() {
         return orcid;
+    }
+
+    public void setTitle(TitleConstants title) {
+        this.title = title;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setOrcid(String orcid) {
+        this.orcid = orcid;
     }
 }

abc83de2d2e5b984e7bc13eaabd620cad2ff744d
Chakradhar Bandla <chakrabandla@gmail.com>
2020-03-28 19:04:22 +0000

    change checksum field name

--- src/main/java/uk/ac/ebi/pride/archive/dataprovider/file/FileProvider.java
+++ src/main/java/uk/ac/ebi/pride/archive/dataprovider/file/FileProvider.java
@@ -61,6 +61,6 @@ public interface FileProvider extends EntityProvider, ParamGroupProvider {
     /**
      * @return Provide an Md5 for each file
      */
-    String getMd5Checksum();
+    String getChecksum();
 
 }
